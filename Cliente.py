c580071ae1976df1110c5708369c17d719f3bdab
jal07 <jcholeiva@gmail.com>
2020-11-11 10:58:19 -0600

    Update Cliente.py

--- Cliente.py
+++ Cliente.py
@@ -1,9 +1,11 @@
 import socket
-import shutil
 import hashlib
 import PySimpleGUI as sg
-from tkinter import *
+import xlrd
 from tkinter import messagebox as MessageBox
+
+#global variable
+messageStatus = 'Seguro'
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,8 +13,6 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('localhost', 10000)
 print('connecting to {} port {}'.format(*server_address))
 sock.connect(server_address)
-
-#shutil.copy(file_path, 'C:/Users/javendano/PycharmProjects/Proyecto1Redes2/Cliente')
 
 layout = [[sg.Text('Mantenga su sistema seguro', size=(30, 1))],
               [sg.Text('Analicemos un archivo', size=(17, 1)), sg.InputText(disabled=bool(True)), sg.FileBrowse()],
@@ -22,7 +22,9 @@ window = sg.Window('ANTIVIRUS 20-20', layout)
 event, values = window.read()
 window.close()
 file_path = values[0]  # get the data from the values dictionary
-print(file_path)
+
+test = xlrd.open_workbook("C:/Users/javendano/PycharmProjects/Proyecto1Redes2/Business/Test.xlsx")
+sh = test.sheet_by_index(0)
 
 with open(file_path, "rb") as f:
     file_hash = hashlib.md5()
@@ -30,21 +32,28 @@ with open(file_path, "rb") as f:
         file_hash.update(chunk)
 
 # print(file_hash.digest())
-print(file_hash.hexdigest())  # to get a printable str instead of bytes
+#print(file_hash.hexdigest())  # to get a printable str instead of bytes
+test = file_hash.hexdigest()
+for rx in range(sh.nrows):
+    if sh.cell(rowx=rx, colx=0).value == file_hash.hexdigest():
+        messageStatus = "Peligroso"
 
+nombre_equipo = socket.gethostname()
+direccion_equipo = socket.gethostbyname(nombre_equipo)
+#print("La IP es: % s" % direccion_equipo)
+messageIP = direccion_equipo
+
+# Send data
+print('sending {!r}'.format(messageIP))
+sock.sendall(messageIP.encode())
+print('sending {!r}'.format(messageStatus))
+sock.sendall(messageStatus.encode())
+print('sending {!r}'.format(test))
+sock.sendall(test.encode())
 
 try:
     seguro = 1
     contador = 0
-
-    # Send data
-    nombre_equipo = socket.gethostname()
-    direccion_equipo = socket.gethostbyname(nombre_equipo)
-    print("La IP es: % s" % direccion_equipo)
-    messageIP = b'This is the message.  It will be repeated.'
-    messageIP = direccion_equipo
-    print('sending {!r}'.format(messageIP))
-    sock.sendall(messageIP.encode())
 
     while contador != 50:
         data = sock.recv(1024)
@@ -55,7 +64,6 @@ try:
 
 finally:
     print('closing socket')
-    print(seguro)
     if seguro == 0:
         print("ADVERTENCIA: El archivo analizado no es seguro y puede significar una amenaza para su equipo")
         MessageBox.showwarning("ADVERTENCIA", "El archivo analizado no es seguro y puede significar una amenaza para su equipo")  # título, mensaje
